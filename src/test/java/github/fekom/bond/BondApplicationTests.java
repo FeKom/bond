e83dbdf13ec4a846b6f31007225275641b6a50f6
fekom <fekomninakis@gmail.com>
2025-10-20 11:10:08 -0300

    first commit

---
+++ src/test/java/github/fekom/bond/BondApplicationTests.java
@@ -0,0 +1,15 @@
+package github.fekom.bond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class BondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
